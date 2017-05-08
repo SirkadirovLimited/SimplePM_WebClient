4c2abcda5e7c5b40e650acad3ae4e40b0a8bacce
Kadirov Yurij <admin@sirkadirov.com>
2017-05-08 21:42:17 +0300

    Дело начато, но со следующего коммита - управление задачами реализовываться будет

--- install/services/main.php
+++ install/services/main.php
@@ -3,25 +3,24 @@
 	
 	include(_header);
 ?>
-<h3>Установка SimplePM_WebClient</h3>
 <div class="progress">
 	<div class="progress-bar progress-bar-success" style="min-width: 3em; width: 0%;">
 		0%
 	</div>
 </div>
-<div class="row">
-	<div class="col-md-3">
-		<ul class="nav nav-pills nav-stacked">
-			<li class="active"><a>Начало установки</a></li>
-			<li><a>Подключение к БД</a></li>
-			<li><a>Первоначальная конфигурация</a></li>
-			<li><a></a></li>
-		</ul>
-	</div>
-	<div class="col-md-9">
-	
-	</div>
+<div style="height: 50%; overflow-y: auto;">
+	<h3>Добро пожаловать!</h3>
+	<p>
+		Добро пожаловать в графический интерфейс установщика SimplePM_WebClient! Данный установщик позволит вам произвести первоначальную 
+		настройку <b>SimplePM_WebClient</b> на вашем веб-сервере. Для продолжения установки нажмите "Далее".
+	</p>
 </div>
+<nav>
+	<ul class="pager">
+		<li class="previous disabled"><a href="#"><span>&larr;</span> Назад</a></li>
+		<li class="next"><a href="#">Далее <span>&rarr;</span></a></li>
+	</ul>
+</nav>
 <?php
 	include(_footer);
 ?>
