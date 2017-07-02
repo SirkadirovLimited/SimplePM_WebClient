8499648566d6d897a08a362b800a8717aad49fa8
Kadirov Yurij <admin@sirkadirov.com>
2017-07-02 22:13:06 +0300

    Обновление "ИЮЛЬ-2-3"
    Изменения по сравнению с предидущим коммитом:
    - Исправлены ошибки, допущенные в предидущем коммите
    - Созданы новые ошибки, которые пофиксятся в последующих коммитах
    - Мы есть Грут!
    - На этом пока-что всё!

--- services/inc/pre-login/login.php
+++ services/inc/pre-login/login.php
@@ -52,10 +52,27 @@
 		if (!((strlen($login)>=3) && (strlen($login)<=100)))
 			exit(header("location: index.php?err=badlogin"));
 		
-		if (!(strlen($pass)>=$_SPM_CONF["PASSWD"]["minlength"]) && !(strlen($pass)<=$_SPM_CONF["PASSWD"]["maxlength"]))
+		$param1 = (strlen($pass)>=$_SPM_CONF["PASSWD"]["minlength"]);
+		$param2 = (strlen($pass)<=$_SPM_CONF["PASSWD"]["maxlength"]);
+		
+		if (!$param1 && !$param2)
 			exit(header("location: index.php?service=login&err=badpass"));
 		
-		if (!$db_result = $db->query("SELECT * FROM `spm_users` WHERE `username` = '$login' AND `password` = '$password' LIMIT 1;"))
+		$query_str = "
+			SELECT
+				*
+			FROM
+				`spm_users`
+			WHERE
+				`username` = '$login'
+			AND
+				`password` = '$password'
+			LIMIT
+				1
+			;
+		";
+		
+		if (!$db_result = $db->query($query_str))
 			exit(header("location: index.php?service=login&err=db"));
 		
 		if ($db_result->num_rows == 0)
@@ -66,10 +83,22 @@
 		$db_result->free();
 		unset($db_result);
 		
-		if ($user['id']>0 && $user['banned'] == 1)
+		if ($user['id'] > 1 && $user['banned'] == 1)
 			exit(header("location: index.php?service=login&err=banned"));
 		
-		if (!$db_result = $db->query("UPDATE `spm_users` SET `online` = '1', `sessionId` = '" . session_id() . "' WHERE `id` = " . $user['id'] . " LIMIT 1;"))
+		$query_str = "
+			UPDATE
+				`spm_users`
+			SET
+				`sessionId` = '" . session_id() . "'
+			WHERE
+				`id` = " . $user['id'] . "
+			LIMIT
+				1
+			;
+		";
+		
+		if (!$db_result = $db->query($query_str))
 			exit(header("location: index.php?service=login&err=db"));
 		
 		$_SESSION['uid'] = $user['id'];
