5a603237868edeb8fa12626326c7a8ee32bc25de
Kadirov Yurij <admin@sirkadirov.com>
2018-03-28 12:36:16 +0300

    Удалена старая версия веб-приложения

--- tpl/default/js/tagsinput/plugin.js
+++
@@ -1,8 +0,0 @@
-$('#tags')[0].addEventListener('input', log);
-$('#tags')[0].addEventListener('change', log);
-function log(e) {
-    $('#out')[0].textContent = `${e.type}: ${this.value.replace(/,/g,', ')}`;
-}
-
-// hook 'em up:
-$('input[type="tags"]').forEach(tagsInput);
--- tpl/default/sidebar/user.inc.php
+++
@@ -1,3 +0,0 @@
-<!--USER-->
-<li <?=spm_isPageCurrent("home")?>><a href="index.php?service=home"><i class="fa fa-home"></i> <span>Головна сторінка</span></a></li>
-<li <?=spm_isPageCurrent("rating")?>><a href="index.php?service=rating"><i class="fa fa-bar-chart"></i> <span>Рейтинг</span></a></li>
