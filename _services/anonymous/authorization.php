6af3a156c00306d9883866f775c4d4b34111b0d8
Kadirov Yurij <admin@sirkadirov.com>
2018-03-28 22:09:31 +0300

    Создан демо-сервис

---
+++ _services/anonymous/authorization.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Copyright (C) 2018, Yurij Kadirov.
+ * All rights are reserved.
+ * Licensed under Apache License 2.0 with additional restrictions.
+ *
+ * @Author: Yurij Kadirov
+ * @Website: https://sirkadirov.com/
+ * @Email: admin@sirkadirov.com
+ * @Repo: https://github.com/SirkadirovTeam/SimplePM_Server
+ */
+
+define("__PAGE_TITLE__", _("Авторизація"));
+define("__PAGE_LAYOUT__", "skeleton");
+
+?>
+<p class="lead">Hi!</p>
