2d8c888ff415a623eb4dbb108bb516de65ebbbd5
Kadirov Yurij <admin@sirkadirov.com>
2018-03-29 20:24:28 +0300

    Коммандный сервис входа пользователя в систему полностью реализован WEBAPP-13

--- _commands/anonymous/login.php
+++ _commands/anonymous/login.php
@@ -11,9 +11,80 @@
  * @Repo: https://github.com/SirkadirovTeam/SimplePM_Server
  */
 
+/*
+ * Запрашиваем  доступ  к
+ * глобальным переменным.
+ */
+
+global $database;
+
+/*
+ * Производим проверку на существование
+ * и не пустоту требуемых полей запроса
+ * на вход в систему.
+ */
+
 Security::CheckPostDataIssetAndNotNull(
     array(
         "username",
         "password"
     )
 ) or Security::ThrowError("input");
+
+/*
+ * Производим выборку найденных по
+ * указанному логину пользователей
+ * (точнее единственного).
+ */
+
+// Создаём шаблон запроса
+$query_str = "
+    SELECT
+      `id`,
+      `password`
+    FROM
+      `spm_users`
+    WHERE
+      `username` = '" . $_POST['username'] . "'
+    LIMIT
+      1
+    ;
+";
+
+// Выполняем запрос по шаблону
+$query = $database->query($query_str);
+
+/*
+ * Проверяем искомого пользователя
+ * на существование по его логину.
+ */
+
+if ($query->num_rows == 0)
+    Security::ThrowError(_("Користувача з вказаним логіном не знайдено!"));
+
+/*
+ * Получаем   объект,  содержащий
+ * необходимые данные о найденном
+ * пользователе   и   присваиваем
+ * ссылку   на   него  специально
+ * созданной переменной.
+ */
+
+$user_obj = $query->fetch_object();
+
+/*
+ * Проверяем  введённый  пароль   на
+ * соответствие с паролем найденного
+ * пользователя.
+ */
+
+if (!password_verify($_POST['password'], $user_obj->password))
+    Security::ThrowError(_("Пароль введено не вірно!"));
+
+/*
+ * Записываем в сессию информацию о
+ * текущем  пользователе,  так  как
+ * вход был проделан ним успешно.
+ */
+
+Security::getCurrentSession()["user_info"] = new SessionUser($user_obj->id);
