21b4bfd2e91ba15b4be806ec1c95838a6400d4d2
Kadirov Yurij <admin@sirkadirov.com>
2018-03-30 13:05:55 +0300

    Начало реализации сервиса пользовательских страниц WEBAPP-13

---
+++ _services/users/profile.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Copyright (C) 2018, Yurij Kadirov.
+ * All rights are reserved.
+ * Licensed under Apache License 2.0 with additional restrictions.
+ *
+ * @Author: Yurij Kadirov
+ * @Website: https://sirkadirov.com/
+ * @Email: admin@sirkadirov.com
+ * @Repo: https://github.com/SirkadirovTeam/SimplePM_Server
+ */
+
+define("__PAGE_TITLE__", _("Профіль користвувача"));
+define("__PAGE_LAYOUT__", "default");
+
+?>
