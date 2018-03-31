3a41bb9898f2817506c7b1b801aa63b6e2bec4e5
Kadirov Yurij <admin@sirkadirov.com>
2018-03-31 18:37:32 +0300

    Продолжается реализация архива задач WEBAPP-13

--- _services/problems/archive.php
+++ _services/problems/archive.php
@@ -32,6 +32,16 @@ $query_str = "
       `spm_problems`.`category_id` = `spm_problems_categories`.`id`
     WHERE
       `spm_problems`.`enabled` = TRUE
+    AND
+      (
+        `spm_problems`.`id` = '" . @(int)$_GET['query'] . "'
+      OR
+        `spm_problems`.`name` LIKE '%" . @$_GET['query'] . "%'
+      )
+    AND
+      (
+        `spm_problems`.`category_id` LIKE '%" . @$_GET['category'] . "%'
+      )
     ORDER BY
       `spm_problems_categories`.`sort` ASC,
       `spm_problems`.`difficulty` ASC
@@ -42,15 +52,56 @@ $problems_list = $database->query($query_str);
 
 ?>
 
+<div class="card">
+    <div class="card-body">
+
+        <form method="get">
+
+            <div class="input-group">
+
+                <div class="input-group-prepend">
+
+                    <select name="category" class="form-control">
+
+                        <option value="" selected><?=_("Виберіть категорію завдань")?></option>
+
+                    </select>
+
+                </div>
+
+                <input
+                    name="query"
+                    type="text"
+                    class="form-control"
+                    placeholder="<?=_("Введіть запит для пошуку...")?>"
+                    value="<?=@$_GET['query']?>"
+                >
+
+                <div class="input-group-prepend">
+
+                    <button type="submit" class="btn btn-primary"><?=_("Знайти")?></button>
+
+                </div>
+
+            </div>
+
+        </form>
+
+    </div>
+</div>
+
 <div class="row" style="margin-top: 2rem;">
 
     <?php while ($problem = $problems_list->fetch_assoc()): ?>
         <div class="col-md-4 col-sm-12" style="margin-bottom: 2rem;">
 
-            <a class="card" href="">
+            <a class="card" style="text-decoration: none !important;" href="">
                 <div class="card-body">
-                    <strong class="card-title"><?=$problem["name"]?></strong>
-                    <p class="card-text"><?=$problem["category_name"]?></p>
+                    <strong class="card-title" style="color: #343a40 !important;"><?=$problem["id"]?>. <?=$problem["name"]?></strong>
+                    <p class="card-text">
+                        <span class="badge badge-info"><?=$problem["category_name"]?></span>
+                        <span class="badge badge-success"><?=$problem["difficulty"]?> points</span>
+                    </p>
                 </div>
             </a>
 
