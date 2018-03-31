eea1dd7f7bbe3b07c05fc971529fb3a09504ca49
Kadirov Yurij <admin@sirkadirov.com>
2018-03-31 19:58:19 +0300

    Мелкие исправления и улучшения + сервис разбит на несколько частей WEBAPP-13

--- _services/problems/archive.php
+++ _services/problems/archive.php
@@ -14,10 +14,26 @@
 define("__PAGE_TITLE__", _("Архів задач"));
 define("__PAGE_LAYOUT__", "default");
 
+/*
+ * Запрашиваем доступ к глобальным переменным
+ */
+
 global $database;
 
-$associated_olymp = Security::getCurrentSession()["user_info"]->getUserInfo()["associated_olymp"];
+/*
+ * Получаем идентификатор текущего соревнования
+ * для возможного ограничения доступного списка
+ * задач.
+ */
 
+$associated_olymp = (int)(Security::getCurrentSession()["user_info"]->getUserInfo()["associated_olymp"]);
+
+/*
+ * Формируем запрос на выборку списка задач
+ * из базы данных и выполняем его.
+ */
+
+// Формируем запрос
 $query_str = "
     SELECT
       `spm_problems`.`id`,
@@ -48,51 +64,16 @@ $query_str = "
     ;
 ";
 
-$problems_list = $database->query($query_str);
+// Выполняем запрос на выборку из БД и получаем массив
+$problems_list = $database->query($query_str)->fetch_all(MYSQLI_ASSOC);
 
 ?>
 
-<div class="card">
-    <div class="card-body">
-
-        <form method="get">
-
-            <div class="input-group">
-
-                <div class="input-group-prepend">
-
-                    <select name="category" class="form-control">
-
-                        <option value="" selected><?=_("Виберіть категорію завдань")?></option>
-
-                    </select>
-
-                </div>
-
-                <input
-                    name="query"
-                    type="text"
-                    class="form-control"
-                    placeholder="<?=_("Введіть запит для пошуку...")?>"
-                    value="<?=@$_GET['query']?>"
-                >
-
-                <div class="input-group-prepend">
-
-                    <button type="submit" class="btn btn-primary"><?=_("Знайти")?></button>
-
-                </div>
-
-            </div>
-
-        </form>
-
-    </div>
-</div>
+<?php include _SPM_views_ . "problems/archive-search-panel.inc"; ?>
 
 <div class="row" style="margin-top: 2rem;">
 
-    <?php while ($problem = $problems_list->fetch_assoc()): ?>
+    <?php foreach ($problems_list as $problem): ?>
         <div class="col-md-4 col-sm-12" style="margin-bottom: 2rem;">
 
             <a class="card" style="text-decoration: none !important;" href="">
@@ -106,6 +87,6 @@ $problems_list = $database->query($query_str);
             </a>
 
         </div>
-    <?php endwhile; ?>
+    <?php endforeach; ?>
 
 </div>
