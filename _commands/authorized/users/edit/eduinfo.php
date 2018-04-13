816804179c73f08e2665759b753cc74220ea0032
Kadirov Yurij <admin@sirkadirov.com>
2018-04-13 21:55:47 +0300

    Начало реализации сервиса редактирования информации о пользователе WEBAPP-13

---
+++ _commands/authorized/users/edit/eduinfo.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Copyright (C) 2018, Yurij Kadirov.
+ * All rights are reserved.
+ * Licensed under Apache License 2.0 with additional restrictions.
+ *
+ * @Author: Yurij Kadirov
+ * @Website: https://sirkadirov.com/
+ * @Email: admin@sirkadirov.com
+ */
+
+/*
+ * Запрашиваем доступ к глобальным переменным
+ */
+
+global $database;
+
+/*
+ * Выполняем различные проверки безопасности
+ */
+
