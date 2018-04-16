7efdbffe3761b8fc049f40a2ad8ab4eaf0f9f6ad
Kadirov Yurij <admin@sirkadirov.com>
2018-04-16 19:23:15 +0300

    Продолжается реализация нового коммандного сервиса WEBAPP-13

--- _commands/authorized/users/edit/eduinfo.php
+++ _commands/authorized/users/edit/eduinfo.php
@@ -20,3 +20,31 @@ global $database;
  * Выполняем различные проверки безопасности
  */
 
+isset($_GET['id']) or Security::ThrowError("input");
+$_GET['id'] = abs((int)$_GET['id']);
+
+UserInfo::UserExists($_GET['id'])
+    or Security::ThrowError("404");
+
+Security::CheckAccessPermissionsForEdit($_GET['id'], true)
+    or Security::ThrowError("403");
+
+/*
+ * Проверяем POST аргументы
+ * на существование.
+ */
+
+Security::CheckPostDataIssetAndNotNull(
+    array(
+        "institution"
+    )
+) or Security::ThrowError("input");
+
+/*
+ * Проверяем правильность заполнения
+ * POST-параметров в форме,  которая
+ * перенаправила   пользователя   на
+ * данный командный сервис.
+ */
+
+strlen_check_post_param("institution", 1, 255);
