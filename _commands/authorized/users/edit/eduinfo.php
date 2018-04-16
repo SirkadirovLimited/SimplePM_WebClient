2c76764782a744695eeb92be558fae8a836f2427
Kadirov Yurij <admin@sirkadirov.com>
2018-04-16 23:12:39 +0300

    Реализована возможность изменять информацию об учебном учреждении, а также изменять теущую группу пользователя WEBAPP-13

--- _commands/authorized/users/edit/eduinfo.php
+++ _commands/authorized/users/edit/eduinfo.php
@@ -48,3 +48,105 @@ Security::CheckPostDataIssetAndNotNull(
  */
 
 strlen_check_post_param("institution", 1, 255);
+
+/*
+ * Обновляем пользовательские данные
+ */
+
+// Формируем запрос на обновление информации
+$query_str = "
+    UPDATE
+      `spm_users`
+    SET
+      `institution` = '" . $_POST['institution'] . "'
+    WHERE
+      `id` = '" . $_GET['id'] . "'
+    LIMIT
+      1
+    ;
+";
+
+// Выполняем запрос на обновление информации
+$database->query($query_str);
+
+/*
+ * РАЗДЕЛ ПРЕДОСТАВЛЕНИЯ ВОЗМОЖНОСТЕЙ ДЛЯ
+ * ИЗМЕНЕНИЯ ТЕКУЩЕЙ ГРУППЫ ПОЛЬЗОВАТЕЛЯ.
+ */
+
+if (Security::CheckAccessPermissionsForEdit($_GET['id'], false))
+{
+
+    /*
+     * Проверяем POST аргументы
+     * на существование.
+     */
+
+    Security::CheckPostDataIssetAndNotNull(
+        array(
+            "groupid"
+        )
+    ) or Security::ThrowError("input");
+
+    $_POST['groupid'] = abs((int)$_POST['groupid']);
+
+    /*
+     * Проверка  на  существование
+     * указанной  пользовательской
+     * группы для обеспечения безо
+     * пасности работы системы.
+     */
+
+    // Формируем запрос на выборку из БД
+    $query_str = "
+        SELECT
+          count(`id`)
+        FROM
+          `spm_users_groups`
+        WHERE
+          `id` = '" . $_POST['groupid'] . "'
+        AND
+          `teacherId` = '" . UserInfo::getUserInfo($_GET['id'])['teacherId'] . "'
+        ;
+    ";
+
+    // Выполняем запрос и обрабатываем результат
+    (int)($database->query($query_str)->fetch_array()[0]) > 0
+        or Security::ThrowError("403");
+
+    /*
+     * Обновляем информацию о пользователе
+     * в базе данных системы.
+     */
+
+    // Формируем запрос на обновление данных
+    $query_str = "
+        UPDATE
+          `spm_users`
+        SET
+          `groupid` = '" . $_POST['groupid'] . "'
+        WHERE
+          `id` = '" . $_GET['id'] . "'
+        LIMIT
+          1
+        ;
+    ";
+
+    // Выполняем запрос на обновление данных
+    $database->query($query_str);
+
+}
+
+/*
+ * Перенаправляем текущего пользователя
+ * на необходимый нам  сервис  системы.
+ */
+
+// Перезаписываем заголовок
+header(
+    'location: ' . _SPM_ . 'index.php/users/profile/?id=' . $_GET['id'],
+    true
+);
+
+// Завершаем работу экземпляра веб-приложения
+exit();
