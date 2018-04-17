0088877b6040a96ada8d0741eecf3346d66d15af
Kadirov Yurij <admin@sirkadirov.com>
2018-04-17 23:12:07 +0300

    Реализована первая версия подсистемы визуального программирования WEBAPP-13

--- _services/authorized/problems/problem.php
+++ _services/authorized/problems/problem.php
@@ -167,7 +167,314 @@ $last_submission_info = @$database->query($query_str)->fetch_assoc();
     }
 </style>
 
-<pre id="code_editor"><?=htmlspecialchars($last_submission_info['problemCode'])?></pre>
+<!-- BLOCKLY INIT -->
+<script src="<?=_SPM_assets_?>_plugins/blockly/blockly_compressed.js"></script>
+<script src="<?=_SPM_assets_?>_plugins/blockly/blocks_compressed.js"></script>
+<script src="<?=_SPM_assets_?>_plugins/blockly/lua_compressed.js"></script>
+
+<script src="<?=_SPM_assets_?>_plugins/blockly/msg/js/uk.js"></script>
+
+<xml id="toolbox" style="display: none">
+    <category name="<?=_("Логіка")?>" colour="%{BKY_LOGIC_HUE}">
+        <block type="controls_if"></block>
+        <block type="logic_compare"></block>
+        <block type="logic_operation"></block>
+        <block type="logic_negate"></block>
+        <block type="logic_boolean"></block>
+        <block type="logic_null"></block>
+        <block type="logic_ternary"></block>
+    </category>
+    <category name="<?=_("Цикли")?>" colour="%{BKY_LOOPS_HUE}">
+        <block type="controls_repeat_ext">
+            <value name="TIMES">
+                <shadow type="math_number">
+                    <field name="NUM">10</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="controls_whileUntil"></block>
+        <block type="controls_for">
+            <value name="FROM">
+                <shadow type="math_number">
+                    <field name="NUM">1</field>
+                </shadow>
+            </value>
+            <value name="TO">
+                <shadow type="math_number">
+                    <field name="NUM">10</field>
+                </shadow>
+            </value>
+            <value name="BY">
+                <shadow type="math_number">
+                    <field name="NUM">1</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="controls_forEach"></block>
+        <block type="controls_flow_statements"></block>
+    </category>
+    <category name="<?=_("Обчислення")?>" colour="%{BKY_MATH_HUE}">
+        <block type="math_number"></block>
+        <block type="math_arithmetic">
+            <value name="A">
+                <shadow type="math_number">
+                    <field name="NUM">1</field>
+                </shadow>
+            </value>
+            <value name="B">
+                <shadow type="math_number">
+                    <field name="NUM">1</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="math_single">
+            <value name="NUM">
+                <shadow type="math_number">
+                    <field name="NUM">9</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="math_trig">
+            <value name="NUM">
+                <shadow type="math_number">
+                    <field name="NUM">45</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="math_constant"></block>
+        <block type="math_number_property">
+            <value name="NUMBER_TO_CHECK">
+                <shadow type="math_number">
+                    <field name="NUM">0</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="math_round">
+            <value name="NUM">
+                <shadow type="math_number">
+                    <field name="NUM">3.1</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="math_on_list"></block>
+        <block type="math_modulo">
+            <value name="DIVIDEND">
+                <shadow type="math_number">
+                    <field name="NUM">64</field>
+                </shadow>
+            </value>
+            <value name="DIVISOR">
+                <shadow type="math_number">
+                    <field name="NUM">10</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="math_constrain">
+            <value name="VALUE">
+                <shadow type="math_number">
+                    <field name="NUM">50</field>
+                </shadow>
+            </value>
+            <value name="LOW">
+                <shadow type="math_number">
+                    <field name="NUM">1</field>
+                </shadow>
+            </value>
+            <value name="HIGH">
+                <shadow type="math_number">
+                    <field name="NUM">100</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="math_random_int">
+            <value name="FROM">
+                <shadow type="math_number">
+                    <field name="NUM">1</field>
+                </shadow>
+            </value>
+            <value name="TO">
+                <shadow type="math_number">
+                    <field name="NUM">100</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="math_random_float"></block>
+    </category>
+    <category name="<?=_("Робота з текстом")?>" colour="%{BKY_TEXTS_HUE}">
+        <block type="text"></block>
+        <block type="text_join"></block>
+        <block type="text_append">
+            <value name="TEXT">
+                <shadow type="text"></shadow>
+            </value>
+        </block>
+        <block type="text_length">
+            <value name="VALUE">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_isEmpty">
+            <value name="VALUE">
+                <shadow type="text">
+                    <field name="TEXT"></field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_indexOf">
+            <value name="VALUE">
+                <block type="variables_get">
+                    <field name="VAR">{textVariable}</field>
+                </block>
+            </value>
+            <value name="FIND">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_charAt">
+            <value name="VALUE">
+                <block type="variables_get">
+                    <field name="VAR">{textVariable}</field>
+                </block>
+            </value>
+        </block>
+        <block type="text_getSubstring">
+            <value name="STRING">
+                <block type="variables_get">
+                    <field name="VAR">{textVariable}</field>
+                </block>
+            </value>
+        </block>
+        <block type="text_changeCase">
+            <value name="TEXT">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_trim">
+            <value name="TEXT">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_print">
+            <value name="TEXT">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_prompt_ext">
+            <value name="TEXT">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+    </category>
+    <category name="<?=_("Списки")?>" colour="%{BKY_LISTS_HUE}">
+        <block type="lists_create_with">
+            <mutation items="0"></mutation>
+        </block>
+        <block type="lists_create_with"></block>
+        <block type="lists_repeat">
+            <value name="NUM">
+                <shadow type="math_number">
+                    <field name="NUM">5</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="lists_length"></block>
+        <block type="lists_isEmpty"></block>
+        <block type="lists_indexOf">
+            <value name="VALUE">
+                <block type="variables_get">
+                    <field name="VAR">{listVariable}</field>
+                </block>
+            </value>
+        </block>
+        <block type="lists_getIndex">
+            <value name="VALUE">
+                <block type="variables_get">
+                    <field name="VAR">{listVariable}</field>
+                </block>
+            </value>
+        </block>
+        <block type="lists_setIndex">
+            <value name="LIST">
+                <block type="variables_get">
+                    <field name="VAR">{listVariable}</field>
+                </block>
+            </value>
+        </block>
+        <block type="lists_getSublist">
+            <value name="LIST">
+                <block type="variables_get">
+                    <field name="VAR">{listVariable}</field>
+                </block>
+            </value>
+        </block>
+        <block type="lists_split">
+            <value name="DELIM">
+                <shadow type="text">
+                    <field name="TEXT">,</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="lists_sort"></block>
+    </category>
+    <sep></sep>
+    <category name="<?=_("Змінні")?>" colour="%{BKY_VARIABLES_HUE}" custom="VARIABLE"></category>
+    <category name="<?=_("Функції")?>" colour="%{BKY_PROCEDURES_HUE}" custom="PROCEDURE"></category>
+</xml>
+
+<!-- /BLOCKLY INIT -->
+
+<nav>
+    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+        <a
+                class="nav-item nav-link active"
+                data-toggle="tab"
+                href="#editor-code"
+                role="tab"
+        ><?=_("Редактор коду")?></a>
+        <a
+                class="nav-item nav-link"
+                data-toggle="tab"
+                href="#editor-visual"
+                role="tab"
+        ><?=_("Візуальний редактор")?></a>
+        <a
+                class="nav-item nav-link disabled"
+                data-toggle="tab"
+                href="#editor-file"
+                onclick=""
+                role="tab"
+        ><?=_("Завантажити файл")?></a>
+
+    </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+    <div
+            class="tab-pane fade show active"
+            id="editor-code"
+            role="tabpanel"
+    >
+
+        <pre id="code_editor"><?=htmlspecialchars($last_submission_info['problemCode'])?></pre>
+
+    </div>
+    <div
+            class="tab-pane fade"
+            id="editor-visual"
+            role="tabpanel"
+    ><div id="blockly-div" style="width: 100%; height: 400px;"></div></div>
+</div>
 
 <form action="<?=_SPM_?>index.php?cmd=problems/send_submission" method="post" enctype="multipart/form-data">
 
@@ -410,3 +717,120 @@ $last_submission_info = @$database->query($query_str)->fetch_assoc();
 
     //editor.session.setMode("ace/mode/c_cpp");
 </script>
+
+<!-- BLOCKLY INIT JS -->
+
+<script>
+
+    function urlBase64Decode(str) // From https://jwt.io/js/jwt.js
+    {
+
+        var output = str.replace(/-/g, '+').replace(/_/g, '/');
+
+        switch (output.length % 4) {
+            case 0:
+                break;
+
+            case 2:
+                output += '==';
+                break;
+            case 3:
+                output += '=';
+                break;
+            default:
+                throw 'Illegal base64url string!';
+        }
+
+        var result = window.atob(output); //polifyll https://github.com/davidchambers/Base64.js
+
+        try {
+            return decodeURIComponent(escape(result));
+        }
+        catch (err) {
+            return result;
+        }
+
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        var blocklyWorkspace = Blockly.inject(
+            'blockly-div',
+            {
+
+                collapse: true,
+                comments: true,
+                sounds: false,
+                toolbox: document.getElementById('toolbox')
+
+            }
+        );
+
+        var blockly_code_checker = (
+
+            ace.edit('code_editor').getValue().startsWith("--[[SIMPLEPM_BLOCKLY_LUA]]--") &&
+
+            $('#language_selector')
+                .val() === "<?=$_CONFIG->getWebappConfig()['blockly_lua_language_name']?>"
+        );
+
+        if (blockly_code_checker)
+        {
+
+            var code = ace.edit('code_editor').getValue();
+            var pos = code.lastIndexOf("--[[XML|") + 8;
+
+            var blocklyXmlCodeBase64 = code.substr(
+                pos,
+                code.length - pos - 8
+            );
+
+            Blockly.Xml.domToWorkspace(
+
+                Blockly.Xml.textToDom(
+
+                    urlBase64Decode(
+                        blocklyXmlCodeBase64
+                    )
+
+                ),
+
+                blocklyWorkspace
+
+            );
+
+        }
+
+        blocklyWorkspace.addChangeListener(function () {
+
+            ace.edit('code_editor').setValue(
+
+                "--[[SIMPLEPM_BLOCKLY_LUA]]--\n"
+                + Blockly.Lua.workspaceToCode(blocklyWorkspace) + "\n"
+                + "--[[XML|" +
+
+                btoa(
+
+                    Blockly.Xml.domToText(
+
+                        Blockly.Xml.workspaceToDom(
+
+                            blocklyWorkspace
+
+                        )
+
+                    )
+
+                )
+
+                + "|XML]]--"
+
+            );
+
+        });
+
+    });
+
+</script>
+
+<!-- /BLOCKLY INIT JS -->
