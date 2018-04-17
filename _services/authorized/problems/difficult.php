9d53d0adfb5c64b13971445d45ba535368bb8c05
Kadirov Yurij <admin@sirkadirov.com>
2018-04-17 23:11:42 +0300

    Доработан сервис отложенных задач WEBAPP-13

--- _services/authorized/problems/difficult.php
+++ _services/authorized/problems/difficult.php
@@ -14,7 +14,9 @@
  * Всевозможные проверки безопасности
  */
 
-isset($_GET['id']) or Security::ThrowError(_("Ідентифікатор задачі не вказано!"));
+isset($_GET['id'])
+    or $_GET['id'] = Security::getCurrentSession()['user_info']->getUserId();
+
 $_GET['id'] = abs((int)$_GET['id']);
 
 /*
@@ -45,6 +47,51 @@ Security::CheckAccessPermissionsForEdit($_GET['id'])
 define("__PAGE_TITLE__", _("Відкладені задачі"));
 define("__PAGE_LAYOUT__", "default");
 
+/*
+ * Запрашиваем доступ к необходимым
+ * глобальным переменным.
+ */
+
+global $database;
+
+/*
+ * Получаем список отложенных
+ * задач для указанного поль-
+ * зователя   и  обрабатываем
+ * эту информацию.
+ */
+
+// Формируем запрос на выборку данных из БД
+$query_str = "
+    SELECT
+      `spm_submissions`.`submissionId`,
+      `spm_submissions`.`problemId`,
+      `spm_submissions`.`time`,
+      `spm_submissions`.`b`,
+      
+      `spm_problems`.`name`,
+      `spm_problems`.`difficulty`
+    FROM
+      `spm_submissions`
+    LEFT JOIN
+      `spm_problems`
+    ON
+      `spm_submissions`.`problemId` = `spm_problems`.`id`
+    WHERE
+      `spm_submissions`.`olympId` = 0
+    AND
+      `spm_submissions`.`userId` = '" . $_GET['id'] . "'
+    AND
+      `spm_submissions`.`b` < `spm_problems`.`difficulty`
+    ORDER BY
+      `spm_submissions`.`time` DESC,
+      `spm_submissions`.`problemId` ASC
+    ;
+";
+
+// Выполняем запрос и обрабатываем результаты
+$difficult_problems = $database->query($query_str)->fetch_all(MYSQLI_ASSOC);
+
 ?>
 
 <div class="card">
@@ -69,9 +116,61 @@ define("__PAGE_LAYOUT__", "default");
 
         </ul>
     </div>
-    <div class="card-body">
+    <div class="card-body" style="padding: 0;">
 
+        <?php if (sizeof($difficult_problems) > 0): ?>
 
+            <table class="table" style="margin: 0;">
+
+                <thead>
+
+                <tr>
+
+                    <th><?=_("ID")?></th>
+                    <th><?=_("Назва задачі")?></th>
+                    <th><?=_("Дата та час відправки")?></th>
+                    <th><?=_("Спроба")?></th>
+
+                </tr>
+
+                </thead>
+
+                <tbody>
+
+                <?php foreach ($difficult_problems as $difficult_problem): ?>
+
+                    <tr>
+
+                        <td><?=$difficult_problem['problemId']?></td>
+                        <td>
+                            <a
+                                class="btn-link"
+                                href="<?=_SPM_?>index.php/problems/problem/?id=<?=$difficult_problem['problemId']?>"
+                            ><?=$difficult_problem['name']?></a>
+                        </td>
+                        <td><?=$difficult_problem['time']?></td>
+                        <td>
+                            <a
+                                class="btn-link"
+                                href="<?=_SPM_?>index.php/problems/result/?id=<?=$difficult_problem['submissionId']?>"
+                            ><?=$difficult_problem['submissionId']?></a>
+                        </td>
+
+                    </tr>
+
+                <?php endforeach; ?>
+
+                </tbody>
+
+            </table>
+
+        <?php else: ?>
+
+            <p class="lead text-success text-center" style="margin: 50px;">
+                <?=_("Відкладених задач не знайдено!")?>
+            </p>
+
+        <?php endif; ?>
 
     </div>
 </div>
