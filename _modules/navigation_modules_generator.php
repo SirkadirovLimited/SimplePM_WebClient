e73317ed1668d5cb9236b0ff58bcd2a1b244258d
Kadirov Yurij <admin@sirkadirov.com>
2018-04-23 17:49:00 +0300

    Меню системы разделено на несколько частей WEBAPP-13

---
+++ _modules/navigation_modules_generator.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Copyright (C) 2018, Yurij Kadirov.
+ * All rights are reserved.
+ * Licensed under Apache License 2.0 with additional restrictions.
+ *
+ * @Author: Yurij Kadirov
+ * @Website: https://sirkadirov.com/
+ * @Email: admin@sirkadirov.com
+ */
+
+?>
+
+<?php if (Olymp::IsAssociatedWithOlymp()): ?>
+
+    <?php include(_SPM_modules_ . "olymp/timer.inc"); ?>
+
+<?php endif; ?>
+
+<?php include(_SPM_modules_ . "navigation_modules/user.inc"); ?>
