8a0784e7d21d6ae863e0c5a43c38ed002aa62bb9
Kadirov Yurij <admin@sirkadirov.com>
2018-04-25 10:35:40 +0300

    Добавлен функционал автоматического продления жизни сессии WEBAPP-12 WEBAPP-13 WEBAPP-15

---
+++ _commands/authorized/session-renew.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Copyright (C) 2018, Yurij Kadirov.
+ * All rights are reserved.
+ * Licensed under Apache License 2.0 with additional restrictions.
+ *
+ * @Author: Yurij Kadirov
+ * @Website: https://sirkadirov.com/
+ * @Email: admin@sirkadirov.com
+ */
+
+exit('OK');
+
+?>
