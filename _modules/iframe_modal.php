a56f3b6c6ab15aa18d0a6ebf7146e5bcbcc94830
Yurij Kadirov <admin@sirkadirov.com>
2018-07-22 22:33:28 +0300

    Початок реалізації WEBAPP-60 - створення файлу та розмітка фрейму

---
+++ _modules/iframe_modal.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * ███████╗██╗███╗   ███╗██████╗ ██╗     ███████╗██████╗ ███╗   ███╗
+ * ██╔════╝██║████╗ ████║██╔══██╗██║     ██╔════╝██╔══██╗████╗ ████║
+ * ███████╗██║██╔████╔██║██████╔╝██║     █████╗  ██████╔╝██╔████╔██║
+ * ╚════██║██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝  ██╔═══╝ ██║╚██╔╝██║
+ * ███████║██║██║ ╚═╝ ██║██║     ███████╗███████╗██║     ██║ ╚═╝ ██║
+ * ╚══════╝╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝╚═╝     ╚═╝     ╚═╝
+ *
+ * SimplePM WebApp is a part of software product "Automated
+ * verification system for programming tasks "SimplePM".
+ *
+ * Copyright 2018 Yurij Kadirov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Visit website for more details: https://spm.sirkadirov.com/
+ */
+
+?>
+
+<div class="modal fade" id="iframe_modal-modal-dialog" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">-=Modal title=-</h5>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" style="padding: 0;">
+                <iframe
+                    frameborder="0"
+                    scrolling="auto"
+                    src=""
+                    style="width: 100%; height: 400px; margin: 0;"
+                    sandbox="allow-same-origin allow-forms allow-top-navigation allow-scripts"
+                ></iframe>
+            </div>
+        </div>
+    </div>
+</div>
